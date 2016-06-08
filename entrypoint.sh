a88f72df60ca3161bde8bc18ff1ddb14d03e5528
ivantichy <ivan.tichy@gmail.com>
2016-06-08 18:31:36 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 service cron start
 certbot certonly --standalone --test-cert --break-my-certs -d jira.ivantichy.cz --renew-by-default --email ivan.tichy@gmail.com --agree-tos --standalone-supported-challenges tls-sni-01
+certbot certonly --standalone --test-cert --break-my-certs -d jenkins.ivantichy.cz --renew-by-default --email ivan.tichy@gmail.com --agree-tos --standalone-supported-challenges tls-sni-01
 exec nginx -g daemon off
