b9bd0c8cecac229b54802f1e786ddde1b891d14d
ivantichy <ivan.tichy@gmail.com>
2016-06-13 14:58:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 
 if [ \! -f  /etc/letsencrypt/live/jira.ivantichy.cz/privkey.pem ]; then
   certbot certonly --standalone --break-my-certs -d jira.ivantichy.cz --renew-by-default --email ivan.tichy@gmail.com --agree-tos --standalone-supported-challenges tls-sni-01
+  openssl dhparam -rand - 2048 >> /etc/letsencrypt/live/jira.ivantichy.cz/cert.pem
 fi
 
 
 if [ \! -f  /etc/letsencrypt/live/jenkins.ivantichy.cz/privkey.pem ]; then
   certbot certonly --standalone --break-my-certs -d jenkins.ivantichy.cz --renew-by-default --email ivan.tichy@gmail.com --agree-tos --standalone-supported-challenges tls-sni-01
+  openssl dhparam -rand - 2048 >> /etc/letsencrypt/live/jenkins.ivantichy.cz/cert.pem
 fi
-
 
 bash -c "/updatecert.sh"
 
