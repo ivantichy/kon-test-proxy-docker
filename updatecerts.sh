46e76d3ce598428c4dde88c9fbd5bd6d0bc67df6
ivantichy <ivan.tichy@gmail.com>
2016-06-14 12:13:22 +0200

    Update updatecerts.sh

--- updatecerts.sh
+++ updatecerts.sh
@@ -3,11 +3,13 @@
 certbot renew --standalone --pre-hook "service nginx stop" --post-hook "service nginx start; echo a" >> /var/log/certupdate.log 2>&1
 
 if [ "`cat /etc/letsencrypt/live/jira.ivantichy.cz/cert.pem | grep -c "BEGIN DH PARAMETERS"`" == "0" ]; then
+  echo "generating DH"
   openssl dhparam -rand - 2048 >> /etc/letsencrypt/live/jira.ivantichy.cz/cert.pem
   service nginx restart
 fi
 
 if [ "`cat /etc/letsencrypt/live/jenkins.ivantichy.cz/cert.pem | grep -c "BEGIN DH PARAMETERS"`" == "0" ]; then
+  echo "generating DH"
   openssl dhparam -rand - 2048 >> /etc/letsencrypt/live/jenkins.ivantichy.cz/cert.pem
   service nginx restart
 fi
