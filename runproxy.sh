38748c4b7542e2bfb2f7323b3ce071f45297e47c
ivantichy <ivan.tichy@gmail.com>
2016-07-26 14:13:58 +0200

    Update runproxy.sh

--- runproxy.sh
+++ runproxy.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run -d -p 80:80 -p 443:443 -v /var/docker-data/letsencrypt/:/etc/letsencrypt/ ivantichy/kon-test-proxy
+docker run -d -p 80:80 -p 443:443 -p 90001:90001 -p 90002:90002 -v /var/docker-data/letsencrypt/:/etc/letsencrypt/ ivantichy/kon-test-proxy
